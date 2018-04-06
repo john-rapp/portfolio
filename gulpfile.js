a465458370b233292e0515eeb73e51515b1b3042
John Rapp <johnrapp814@gmail.com>
2018-04-06 13:18:05 -0400

    Renamed variables.scss to _variables.scss, simplified color scheme, implemented column names for CSS grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 "use strict";
+
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("sass", function() {
   return gulp
     .src("./sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass.sync().on("error", sass.logError))
     .pipe(gulp.dest("./css"));
 });
 
