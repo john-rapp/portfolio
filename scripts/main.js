1601ae3dabe0c2dc05b431d2f57dbc470452e6aa
John Rapp <johnrapp814@gmail.com>
2018-06-22 13:27:33 -0400

    Removed unnecessary animation, reduced image sizes, added javascript to "lazy load" images (will need to improve on this later).

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
 function scrollToSection(el) {
   document.querySelector(el).scrollIntoView({
     behavior: 'smooth',
-    block: 'start',
+    block: 'start'
   });
 }
+
+let images = [...document.querySelectorAll('[data-src]')];
+images.forEach(img => {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+    img.removeAttribute('data-src');
+  };
+});
