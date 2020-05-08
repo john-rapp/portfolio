24a48cab562419d6c33a98da795cf2bfaaa8eaa5
john-rapp <johnrapp814@gmail.com>
2020-05-08 19:17:19 -0400

    mobile view stutter fix

--- src/index.js
+++ src/index.js
@@ -11,8 +11,3 @@ images.forEach((img) => {
     img.removeAttribute("data-src");
   });
 });
-
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
