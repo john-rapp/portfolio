e00c6b2e2e3ab59f527bf8ecdd5152577e9dc5ea
john-rapp <johnrapp814@gmail.com>
2020-07-07 09:37:16 -0400

    redesigning projects section
    fixing eslint warnings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import "./styles/app/scss/styles.scss";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./images/color-game.png";
-import "./images/portfolio.png";
+import './styles/app/scss/styles.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './images/color-game.png';
+import './images/portfolio.png';
+import './images/account-overview.png';
 
-const images = [...document.querySelectorAll("[data-src]")];
+const images = [...document.querySelectorAll('[data-src]')];
 
 images.forEach((img) => {
-  img.setAttribute("src", img.getAttribute("data-src"));
-  img.addEventListener("load", () => {
-    img.removeAttribute("data-src");
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.addEventListener('load', () => {
+    img.removeAttribute('data-src');
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ['./src/index.js'],
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
-      favicon: "./src/images/favicon.ico",
+      template: './src/templates/index.html',
+      favicon: './src/images/favicon.ico',
     }),
     new CleanWebpackPlugin(),
   ],
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     hot: true,
-    host: "192.168.1.10",
     liveReload: true,
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
               esModule: false,
             },
@@ -45,11 +44,11 @@ module.exports = {
       },
       {
         test: /\.(woff[2]?|ttf|eot)$/i,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.html$/i,
-        use: ["html-loader"],
+        use: ['html-loader'],
       },
     ],
   },
